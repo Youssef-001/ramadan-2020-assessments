dea23e5df4a1b48cdaaa51849f480aa869a19c9f
Youssef-001 <youssefsamir432@gmail.com>
2024-09-10 07:47:00 +0300

    Task 9 done
     Make a super user capabilities, delete, add resolution video, and change status. all are only visible to him.

--- client.js
+++ client.js
@@ -1,14 +1,44 @@
 const listOfVidsElem = document.getElementById("listOfRequests");
+const SUPER_USER_ID = "28805";
 
 const state = {
   sortBy: "newFirst",
   searchTerm: "",
   userId: "",
+  is_super_user: false,
 };
 
 function renderSingleVid(vidInfo, isPrepend = false) {
   const vidReqContainer = document.createElement("div");
   vidReqContainer.innerHTML = `        <div class="card mb-3">
+
+            ${
+              state.is_super_user
+                ? `<div class="card-header d-flex justify-content-between">
+          <select id="admin_change_status_${vidInfo._id}">
+            <option value="new">new</option>
+            <option value="planned">planned</option>
+            <option value="done">done</option>
+          </select>
+          <div class="input-group ml-2 mr-5 ${
+            vidInfo.status != "done" ? "d-none" : ""
+          }" id = "admin_video_res_container_${vidInfo._id}">
+            <input type="text" class="form-control"
+              id="admin_video_res_${vidInfo._id}" 
+              placeholder="paste here youtube video id">
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" 
+                id="admin_save_video_res_${vidInfo._id}"
+                type="button">Save</button>
+            </div>
+          </div>
+          <button id="admin_delete_video_req_${
+            vidInfo._id
+          }" class='btn btn-danger'>delete</button>
+        </div>`
+                : ""
+            }
+    
           <div class="card-body d-flex justify-content-between flex-row">
             <div class="d-flex flex-column">
               <h3>${vidInfo.topic_title}</h3>
@@ -45,6 +75,69 @@ function renderSingleVid(vidInfo, isPrepend = false) {
   if (isPrepend) listOfVidsElem.prepend(vidReqContainer);
   else listOfVidsElem.appendChild(vidReqContainer);
 
+  const admin_change_status_elem = document.getElementById(
+    `admin_change_status_${vidInfo._id}`
+  );
+
+  const admin_vid_res_elem = document.getElementById(
+    `admin_video_res_${vidInfo._id}`
+  );
+
+  const admin_vid_res_container = document.getElementById(
+    `admin_video_res_container_${vidInfo._id}`
+  );
+
+  const admin_save_vid_res_elem = document.getElementById(
+    `admin_save_video_res_${vidInfo._id}`
+  );
+
+  const admin_delete_video_request_elem = document.getElementById(
+    `admin_delete_video_req_${vidInfo._id}`
+  );
+
+  if (state.is_super_user) {
+    admin_change_status_elem.value = vidInfo.status;
+    admin_vid_res_elem.value = vidInfo.video_ref.link;
+
+    admin_change_status_elem.addEventListener("change", (e) => {
+      if (e.target.value == "done") {
+        admin_vid_res_container.classList.remove("d-none");
+      } else {
+        updateVideoStatus(vidInfo._id, e.target.value);
+      }
+    });
+
+    admin_save_vid_res_elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (!admin_vid_res_elem.value) {
+        admin_vid_res_elem.classList.add("is-invalid");
+        admin_vid_res_elem.addEventListener("input", (e) =>
+          admin_vid_res_elem.classList.remove("is-invalid")
+        );
+        return;
+      }
+
+      updateVideoStatus(vidInfo._id, "done", admin_vid_res_elem.value);
+    });
+
+    admin_delete_video_request_elem.addEventListener("click", (e) => {
+      e.preventDefault();
+      const is_sure = confirm(
+        `Are you sure you want to delete ${vidInfo.topic_title}`
+      );
+
+      if (!is_sure) return;
+      fetch("http://localhost:7777/video-request", {
+        method: "DELETE",
+        headers: { "content-Type": "application/json" },
+        body: JSON.stringify({
+          id: vidInfo._id,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => window.location.reload());
+    });
+  }
   applyVoteStyle(vidInfo._id, vidInfo["votes"]);
 
   const voteUpElem = document.getElementById(`votes_ups_${vidInfo._id}`);
@@ -55,7 +148,24 @@ function renderSingleVid(vidInfo, isPrepend = false) {
     `[id^=votes_][id$=_${vidInfo._id}]`
   );
 
+  function updateVideoStatus(id, status, resVideo = "") {
+    fetch("http://localhost:7777/video-request", {
+      method: "PUT",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify({
+        id,
+        status,
+        resVideo,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => window.location.reload());
+  }
+
   votesElems.forEach((elem) => {
+    if (state.is_super_user) {
+      return;
+    }
     elem.addEventListener("click", function (e) {
       e.preventDefault();
 
@@ -81,6 +191,17 @@ function renderSingleVid(vidInfo, isPrepend = false) {
 }
 
 function applyVoteStyle(video_id, votes_list, vote_type) {
+  const voteUpElem = document.getElementById(`votes_ups_${video_id}`);
+  const voteDownElem = document.getElementById(`votes_downs_${video_id}`);
+
+  if (state.is_super_user) {
+    voteUpElem.style.opacity = "0.5";
+    voteDownElem.style.opacity = "0.5";
+    voteDownElem.style.cursor = "not-allowed";
+    voteUpElem.style.cursor = "not-allowed";
+
+    return;
+  }
   if (!vote_type) {
     if (votes_list.ups.includes(state.userId)) vote_type = "ups";
     else if (votes_list.downs.includes(state.userId)) vote_type = "downs";
@@ -88,8 +209,6 @@ function applyVoteStyle(video_id, votes_list, vote_type) {
       return;
     }
   }
-  const voteUpElem = document.getElementById(`votes_ups_${video_id}`);
-  const voteDownElem = document.getElementById(`votes_downs_${video_id}`);
   const voteDirectionElem = vote_type == "ups" ? voteUpElem : voteDownElem;
 
   const voteOtherElem = vote_type == "ups" ? voteDownElem : voteUpElem;
@@ -164,6 +283,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (window.location.search) {
     state.userId = new URLSearchParams(window.location.search).get("id");
+    if (state.userId == SUPER_USER_ID) {
+      state.is_super_user = true;
+      document.querySelector(".normal_user_content").classList.add("d-none");
+    }
     console.log(state.userId);
     formLoginElm.classList.add("d-none");
     appContentElm.classList.remove("d-none");
